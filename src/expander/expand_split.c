c753116b7227884024e89e603567db53b2bd56ec
juan-cas <jj-haberkorn@proton.me>
2024-12-05 18:43:59 +0100

    more reformating again

--- src/expander/expand_split.c
+++ src/expander/expand_split.c
@@ -1,6 +1,23 @@
 #include "../../minishell.h"
 
-// TODO: Aqu ambas funciones se pasan de 25 xd
+//TODO: verificar que liberan las dos funciones auxiliares
+
+static void *free_word_split(char **split_words, t_cmd_name *new)
+{
+	cleanup(split_words);
+	ft_lstclear((t_list **)&new, free);
+	return (NULL);
+}
+
+static void *free_redir_split(char **split_words, t_cmd_red *new)
+{
+	cleanup(split_words);
+	if (new->content)
+		free(new->content);
+	if (new)
+		free(new);
+	return (NULL);
+}
 
 t_cmd_red	*expand_split_redir(char *name)
 {
@@ -13,32 +30,19 @@ t_cmd_red	*expand_split_redir(char *name)
 	if (!split_words)
 		return (NULL);
 	exp = NULL;
-	i = 0;
-	while (split_words[i])
+	i = -1;
+	while (split_words[++i])
 	{
 		new = ft_calloc(1, sizeof(t_cmd_red));
 		if (!new)
-		{
-			cleanup(split_words);
-			return (NULL);
-		}
+			return (free_redir_split(split_words, new));
 		new->content = ft_calloc(1, sizeof(t_cmd_dred));
 		if (!new->content)
-		{
-			cleanup(split_words);
-			free(new);
-			return (NULL);
-		}
+			return (free_redir_split(split_words, new));
 		new->content->where = ft_strdup(split_words[i]);
 		if (!new->content->where)
-		{
-			cleanup(split_words);
-			free(new->content);
-			free(new);
-			return (NULL);
-		}
+			return (free_redir_split(split_words, new));
 		ft_lstadd_back((t_list **)&exp, (t_list *)new);
-		i++;
 	}
 	cleanup(split_words);
 	return (exp);
@@ -60,18 +64,10 @@ t_cmd_name	*expand_split_word(char *name)
 	{
 		new = ft_calloc(1, sizeof(t_cmd_name));
 		if (!new)
-		{
-			cleanup(split_words);
-			ft_lstclear((t_list **)&new, free);
-			return (NULL);
-		}
+			return (free_word_split(split_words, new));
 		new->name = ft_strdup(split_words[i]);
 		if (!new->name)
-		{
-			cleanup(split_words);
-			ft_lstclear((t_list **)&new, free);
-			return (NULL);
-		}
+			return (free_word_split(split_words, new));
 		ft_lstadd_back((t_list **)&exp, (t_list *)new);
 		i++;
 	}
